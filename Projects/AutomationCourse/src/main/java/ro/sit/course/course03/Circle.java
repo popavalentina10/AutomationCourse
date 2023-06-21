0f7ae1124f18369e31e730e7220ce4cd6131b42d
popavalentina10 <popa_valentina10@yahoo.com>
2023-06-22 00:08:09 +0300

    Add missing courses and related files

---
+++ Projects/AutomationCourse/src/main/java/ro/sit/course/course03/Circle.java
@@ -0,0 +1,15 @@
+package ro.sit.course.course03;
+
+public class Circle {
+
+    double radius;
+
+
+    void setRadius(double rad) {
+        radius = rad;
+    }
+
+    double getArea() {
+        return 3.14 * (radius * radius);
+    }
+}
---
+++ Projects/AutomationCourse/src/main/java/ro/sit/course/course03/Person.java
@@ -0,0 +1,11 @@
+package ro.sit.course.course03;
+
+public class Person {
+    String name;
+    byte age;
+    boolean hungry;
+
+    public void eat() {
+        hungry = false;
+    }
+}
